6e23d6bffc4fcd8e1f8669c27b6d69f6bd8b851f
tun <bt5100@leto.eonet.ne.jp>
2021-10-24 16:44:41 +0900

    ctr user_title 修正

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -25,7 +25,7 @@ class TitlesController < ApplicationController
 
       def user_title
 
-        if Content.where(sub:content_params[:sub]).empty?
+        if Content.where(sub:content_params[:sub]).where(switch:1).empty?
         render json: [0]
         else
         render json: [1]
