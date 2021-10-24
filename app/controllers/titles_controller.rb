f55000b5cd12f5480a70ee7e8740a2db4e9efe14
tun <bt5100@leto.eonet.ne.jp>
2021-10-24 18:53:31 +0900

    empty

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -25,7 +25,7 @@ class TitlesController < ApplicationController
 
       def user_title
 
-        if Content.where(sub:content_params[:sub]).where(switch:1).present?
+        if Content.where(sub:content_params[:sub]).where(switch:1).empty?
         render json: [0]
         else
         render json: [1]
