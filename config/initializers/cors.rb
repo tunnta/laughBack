b46f18aac4e8f72a27c983ab9c66eae5e99aa7b5
tun <bt5100@leto.eonet.ne.jp>
2021-10-24 17:23:31 +0900

    /を取り除いてみる

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'https://laugh.vercel.app/'
+     origins 'https://laugh.vercel.app'
 
      resource '*',
        headers: :any,
