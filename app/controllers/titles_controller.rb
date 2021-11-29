37c825588f0174353cfbb9d1604794fa2a641781
tun <bt5100@leto.eonet.ne.jp>
2021-11-29 17:55:31 +0900

    X日経過したものの集計

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -1,97 +1,85 @@
 class TitlesController < ApplicationController
   
 
-      def create
-   
-        Content.new(content_params).save
-        
-                
-      end
+  def create
+    Content.new(content_params).save
+  end
 
-      def create_good
-        
-         #good_count = GoodUser.where(answer_id:good_params[:answer_id]).count
-        
-        GoodUser.new(good_params).save        
-      end
+  def create_good
+     #good_count = GoodUser.where(answer_id:good_params[:answer_id]).count
+    GoodUser.new(good_params).save        
+  end
 
+  def answer
+    Answer.new(answer_params).save        
+  end
 
-      def answer
-   
-        Answer.new(answer_params).save
-        
-                
-      end
+  def user_title
 
-      def user_title
+    if Content.where(sub:content_params[:sub]).where(switch:1).empty?
+      render json: [1]
+    else
+      render json: [0]
+    end
+  end
 
-        if Content.where(sub:content_params[:sub]).where(switch:1).empty?
-        render json: [0]
-        else
-        render json: [1]
-        end
-      end
+  def send_to_client0
+    if content_params.empty? && Content.where(switch:0).blank?
+       render json: [title:"emp",size:200,id:4]
+    else
+      @content = Content.where(switch:0).limit(20)
+      render json: @content
+    end
+  end
 
-      def send_to_client0
+  def send_to_client
+    #パラメーターをフロントから送った場合とそうじゃない場合
+    if content_params.empty? && Content.where(switch:1).empty?
+      render json: [title:"emp",size:200,id:"emp"]
 
-        if content_params.empty? && Content.where(switch:0).blank?
+    elsif content_params.empty?
+      @content = Content.where(switch:1).order("RANDOM()")
+      render json: @content.select("title","id","size")
 
-          render json: [title:"emp",size:200,id:4]
-        else
+    elsif content_params[:id] == "emp"
+      render json: [answer: "emp"]
 
-        @content = Content.where(switch:0).limit(20)
+    elsif content_params[:sub] != "emp" && content_params[:switch] == "1"          
+      @answer = Answer.where(content_id:content_params[:id])
+      @answer = @answer.map { |answer|
+                              if GoodUser.where(answer_id: answer.id).where(sub: content_params[:sub]).empty? 
+                                good_user = "gray"
+                              else
+                                good_user = "blue"
+                              end
+                            {id: answer[:id],answer: answer[:answer],content_id: answer[:content_id],good: good_user}
+                         }
 
-        render json: @content
+      render json: [Content.where(id: content_params[:id]).where(switch:1).select("title","id","size") ,@answer]
+    
+    elsif content_params[:switch] == "1"  
+       render json: [Content.where(id: content_params[:id]).where(switch:1).select("title","id","size") ,Answer.where(content_id:content_params[:id]).select("answer")]
 
-        end
+    elsif content_params[:switch] == "0" && Answer.where(content_id:content_params[:id]).empty?
+      render json: [Content.where(id: content_params[:id]).where(switch:0).select("title","id","size") ,[answer: "emp"]]
 
-      end
-
-      def send_to_client
-        #パラメーターをフロントから送った場合とそうじゃない場合
-
-        if content_params.empty? && Content.blank?
-
-          render json: [title:"emp",size:200,id:0]
-
-        elsif content_params.empty?
-
-        @content = Content.where(switch:1).order("RANDOM()").limit(5)
-        render json: @content.select("title","id","size")
-
-        elsif content_params[:sub] && content_params[:sub] != "emp"
+    elsif content_params[:switch] == "0"
+      @answer = Answer.where(content_id:content_params[:id]).select("answer","id")
+      @answer = @answer.map{|answer,index|
+      cou = GoodUser.where(answer_id: answer[:id]).count
+      {id: answer[:id],answer: answer[:answer],count: cou}
+      @answer = @answer.sort {|a, b| b[:count] <=> a[:count] }
+        render json: [Content.where(id: content_params[:id]).where(switch:0).select("title","id","size") ,@answer]
           
-          @answer = Answer.where(title_id:content_params[:id])
+    end
+  end
 
-         @answer = @answer.map { |answer|
-                                  if GoodUser.where(answer_id: answer.id).where(sub: content_params[:sub]).empty? 
-                                    good_user = "gray"
-                                  else
-                                    good_user = "blue"
-                                  end
-                                {id: answer[:id],answer: answer[:answer],title_id: answer[:title_id],good: good_user}
-                             }
+  def content_params
+    params.permit(:title, :sub, :size, :id, :switch)
+  end
+
+  def answer_params
+    params.permit(:content_id, :answer, :user_sub)
+  end
   
-            render json: [Content.where(id: content_params[:id]).where(switch:1).select("title","id","size") ,@answer]
-        
-        else
-          
-           render json: [Content.where(id: content_params[:id]).where(switch:1).select("title","id","size") ,Answer.where(title_id:content_params[:id])]
-        end
-      end
-
-
-      def content_params
-        params.permit(:title, :sub, :size, :id, :switch)
-      end
-
-      def answer_params
-        params.permit(:title_id, :answer, :user_sub)
-      end
-
-      def good_params
-        params.permit(:sub,:answer_id)
-      end
-
-      
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   post '/title', to: 'titles#create'
   get '/user_title/:sub',to: 'titles#user_title'
   get '/title', to: 'titles#send_to_client'
-  get '/title/:id/:sub', to: 'titles#send_to_client'
+  get '/title/:id/:sub/:switch', to: 'titles#send_to_client'
   get '/title/:id', to: 'titles#send_to_client'
   get '/title_content0', to: 'titles#send_to_client0'
   post '/answer', to: 'titles#answer'
